f796296d7a827be52a28c58041f5669740c6c723
mashaal <omar@teacups.io>
2017-02-07 08:05:44 +1100

    update green

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const green = '#55a087'
 const yellow = '#ce839d'
 const blue = '#65d7fd'
 const magenta = yellow
-const cyan = green
+const cyan = '#87ffaf'
 
 exports.decorateConfig = config => Object.assign({}, config, {
   backgroundColor,
