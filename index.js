aa371541fb2b0192e822fd2e1acb384cac1806e7
omar mashaal <omar@teacups.io>
2018-10-26 15:54:55 +1100

    remove legacy css code

--- index.js
+++ index.js
@@ -7,41 +7,31 @@ const blue = '#65d7fd'
 const magenta = yellow
 const cyan = green
 
-exports.decorateConfig = config => Object.assign({}, config, {
-  backgroundColor,
-  foregroundColor,
-  borderColor: '#212121',
-  cursorColor: '#97979b',
-  colors: {
-    black: backgroundColor,
-    red,
-    green,
-    yellow,
-    blue,
-    magenta,
-    cyan,
-    white: '#f1f1f0',
-    lightBlack: '#686868',
-    lightRed: red,
-    lightGreen: green,
-    lightYellow: yellow,
-    lightBlue: blue,
-    lightMagenta: magenta,
-    lightCyan: cyan,
-    lightWhite: foregroundColor
-  },
-  css: `
+exports.decorateConfig = config =>
+  Object.assign({}, config, {
+    backgroundColor,
+    foregroundColor,
+    borderColor: '#212121',
+    cursorColor: '#97979b',
+    colors: {
+      black: backgroundColor,
+      red,
+      green,
+      yellow,
+      blue,
+      magenta,
+      cyan,
+      white: '#f1f1f0',
+      lightBlack: '#686868',
+      lightRed: red,
+      lightGreen: green,
+      lightYellow: yellow,
+      lightBlue: blue,
+      lightMagenta: magenta,
+      lightCyan: cyan,
+      lightWhite: foregroundColor
+    },
+    css: `
     ${config.css}
-    .tab_tab:not(.tab_active) {
-      color: hsl(0, 0%, 50%);
-    }
-    .term_fit:not(.term_term) {
-      opacity: 0.5;
-    }
-    .term_fit.term_active {
-      opacity: 1;
-      transition: opacity 0.12s ease-in-out;
-      will-change: opacity;
-    }
   `
-})
+  })
